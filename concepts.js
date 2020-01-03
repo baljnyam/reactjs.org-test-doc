ce9647f740f71ead8dc04766b716f967031b6491
baljnyam <baljnyam@gmail.com>
2020-01-03 18:34:36 +1100

    handle

--- concepts.js
+++ concepts.js
@@ -232,38 +232,64 @@ class Clock extends React.Component {
 // ReactDOM.render(<Clock />, document.getElementById("root"));
 
 //States
-function FormattedDate(props) {
-  return <h2>It is {props.date.toLocaleTimeString()}.</h2>;
-}
+// function FormattedDate(props) {
+//   return <h2>It is {props.date.toLocaleTimeString()}.</h2>;
+// }
 
-class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { date: new Date() };
-  }
+// class Clock extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { date: new Date() };
+//   }
 
-  componentDidMount() {
-    this.timerID = setInterval(() => this.tick(), 1000);
-  }
+//   componentDidMount() {
+//     this.timerID = setInterval(() => this.tick(), 1000);
+//   }
 
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
+//   componentWillUnmount() {
+//     clearInterval(this.timerID);
+//   }
 
-  tick() {
-    this.setState({
-      date: new Date()
-    });
-  }
+//   tick() {
+//     this.setState({
+//       date: new Date()
+//     });
+//   }
 
-  render() {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <FormattedDate date={this.state.date} />
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div>
+//         <h1>Hello, world!</h1>
+//         <FormattedDate date={this.state.date} />
+//       </div>
+//     );
+//   }
+// }
 
-ReactDOM.render(<Clock />, document.getElementById("root"));
+// ReactDOM.render(<Clock />, document.getElementById("root"));
+
+// class Toggle extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { isToggleOn: true };
+
+//     // This binding is necessary to make `this` work in the callback
+//     this.handleClick = this.handleClick.bind(this);
+//   }
+
+//   handleClick() {
+//     this.setState(prevState => ({
+//       isToggleOn: !prevState.isToggleOn
+//     }));
+//   }
+
+//   render() {
+//     return (
+//       <button onClick={this.handleClick}>
+//         {this.state.isToggleOn ? "ON" : "OFF"}
+//       </button>
+//     );
+//   }
+// }
+
+// ReactDOM.render(<Toggle />, document.getElementById("root"));

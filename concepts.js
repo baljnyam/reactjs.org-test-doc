37a65c6af10687c1de7b38e0326343a6731cf1b3
baljnyam <baljnyam@gmail.com>
2020-01-03 18:23:46 +1100

    concept

--- concepts.js
+++ concepts.js
@@ -66,18 +66,204 @@
 //   document.getElementById('root')
 // )
 
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
-}
+// function Welcome(props) {
+//   return <h1>Hello, {props.name}</h1>;
+// }
 
-function App() {
+// function App() {
+//   return (
+//     <div>
+//       <Welcome name="Sara" />
+//       <Welcome name="Darius" />
+//       <Welcome name="Danny" />
+//     </div>
+//   );
+// }
+
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+// It is possible to write this way, but bit complicated
+// function formatDate(date) {
+//   return date.toLocaleDateString();
+// }
+
+// function Comment(props) {
+//   return (
+//     <div className="Comment">
+//       <div className="UserInfo">
+//         <img src="{props.author.avatorUrl}" alt="{props.author.name}" />
+//         <div className="UserInfo-name">{props.author.name}</div>
+//       </div>
+//       <div className="Comment-text">{props.text}</div>
+//       <div className="Comment-date">{formatDate(props.date)}</div>
+//     </div>
+//   );
+// }
+
+// const comment = {
+//   date: new Date(),
+//   text: "I hope you enjoy learning React!",
+//   author: {
+//     name: "Hello Kitty",
+//     avatarUrl: "https://placekitten.com/g/64/64"
+//   }
+// };
+
+// ReactDOM.render(
+//   <Comment date={comment.date} text={comment.text} author={comment.author} />,
+//   document.getElementById("root")
+// );
+
+// bit extraction needed
+
+// function formatDate(date) {
+//   return date.toLocaleDateString();
+// }
+
+// function Avatar(props) {
+//   return (
+//     <img className="Avatar" src={props.user.avatarUrl} alt={props.user.name} />
+//   );
+// }
+
+// function UserInfo(props) {
+//   return (
+//     <div className="UserInfo">
+//       <Avatar user={props.user} />
+//       <div className="UserInfo-name">{props.user.name}</div>
+//     </div>
+//   );
+// }
+
+// function Comment(props) {
+//   return (
+//     <div className="Comment">
+//       <UserInfo user={props.author} />
+//       <div className="Comment-text">{props.text}</div>
+//       <div className="Comment-date">{formatDate(props.date)}</div>
+//     </div>
+//   );
+// }
+
+// const comment = {
+//   date: new Date(),
+//   text: "I hope you enjoy learning React!",
+//   author: {
+//     name: "Hello Kitty",
+//     avatarUrl: "https://placekitten.com/g/64/64"
+//   }
+// };
+// ReactDOM.render(
+//   <Comment date={comment.date} text={comment.text} author={comment.author} />,
+//   document.getElementById("root")
+// );
+
+// All React components must act like pure functions with respect to their props.
+
+// function tick() {
+//   const element = (
+//     <div>
+//       <h1>Hello, World!</h1>
+//       <h2>It is {new Date().toLocaleDateString()}.</h2>
+//     </div>
+//   );
+//   ReactDOM.render(element, document.getElementById("root"));
+// }
+
+// setInterval(tick, 1000);
+function Clock(props) {
   return (
     <div>
-      <Welcome name="Sara" />
-      <Welcome name="Darius" />
-      <Welcome name="Danny" />
+      <h1>Hello, world!</h1>
+      <h2>It is {props.date.toLocaleTimeString()}.</h2>
     </div>
   );
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+function tick() {
+  ReactDOM.render(<Clock date={new Date()} />, document.getElementById("root"));
+}
+
+setInterval(tick, 1000);
+/*
+1. reate an ES6 class, with the same name, that extends React.Component.
+
+2. Add a single empty method to it called render().
+
+3. Move the body of the function into the render() method.
+
+4. Replace props with this.props in the render() body.
+
+5. Delete the remaining empty function declaration.
+*/
+
+class Clock extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, her is the function to class</h1>
+        <h2>
+          That's it. We need states
+          {this.props.date.toLocaleTimeString()}.
+        </h2>
+      </div>
+    );
+  }
+}
+
+// Local state to class
+
+// class Clock extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { date: new Date() };
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>Hello, world!</h1>
+//         <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+//       </div>
+//     );
+//   }
+// }
+
+// ReactDOM.render(<Clock />, document.getElementById("root"));
+
+//States
+function FormattedDate(props) {
+  return <h2>It is {props.date.toLocaleTimeString()}.</h2>;
+}
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <FormattedDate date={this.state.date} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Clock />, document.getElementById("root"));

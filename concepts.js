183a1f032a36890dc8a316d31a5b303da2bbef49
baljnyam <baljnyam@gmail.com>
2020-01-04 15:24:01 +1100

    concept

--- concepts.js
+++ concepts.js
@@ -1,176 +1,177 @@
-// const name = "Hey Man";
-// const element = <h1>Hello, {name}</h1>;
+const name = "Hey Man";
+const element = <h1>Hello, {name}</h1>;
 
-// ReactDOM.render(element, document.getElementById("root"));
+ReactDOM.render(element, document.getElementById("root"));
 
-// function formatName(user) {
-//   return user.firstName + " " + user.lastName;
-// }
+function formatName(user) {
+  return user.firstName + " " + user.lastName;
+}
 
-// const user = {
-//   firstName: "Harper",
-//   lastName: "Perez"
-// };
+const user = {
+  firstName: "Harper",
+  lastName: "Perez"
+};
 
-// function getGreeting(user) {
-//   if (user) {
-//     return <h1> Hello, {formatName(user)}!</h1>;
-//   }
-//   return <h1>Hello, Stranger.</h1>;
-// }
+function getGreeting(user) {
+  if (user) {
+    return <h1> Hello, {formatName(user)}!</h1>;
+  }
+  return <h1>Hello, Stranger.</h1>;
+}
 
-// const element = (
-//   <h1>
-//     Hello, {formatName(user)}!, {getGreeting(user)}
-//   </h1>
-// );
+const element = (
+  <h1>
+    Hello, {formatName(user)}!, {getGreeting(user)}
+  </h1>
+);
 
-// const element = <div tabIndex="0"></div>;
-// const element = <img src={user.avatarUrl} />;
-// const element = (
-//   <div>
-//     <h1>Hello!</h1>
-//     <h2>Good to see you here</h2>
-//   </div>
-// );
+const element = <div tabIndex="0"></div>;
+const element = <img src={user.avatarUrl} />;
+const element = (
+  <div>
+    <h1>Hello!</h1>
+    <h2>Good to see you here</h2>
+  </div>
+);
 
-// const title = response.potentiallyMaliciousInput;
-// // This is safe
-// const element = <h1>{title}</h1>;
+const title = response.potentiallyMaliciousInput;
+// This is safe
+const element = <h1>{title}</h1>;
 
-// ReactDOM.render(element, document.getElementById("root"));
-// Identical
-// const element = <h1 className="greeting">Hello, world!</h1>;
+ReactDOM.render(element, document.getElementById("root"));
+Identical
+const element = <h1 className="greeting">Hello, world!</h1>;
 
-// const element = React.createElement(
-//   "h1",
-//   { className: "greeting" },
-//   "Hello, world"
-// );
+const element = React.createElement(
+  "h1",
+  { className: "greeting" },
+  "Hello, world"
+);
 
-// function and class comonents
+function and class comonents
 
-// function Welcome(props) {
-//   return <h1> Hello, {props.name} </h1>;
-// }
+function Welcome(props) {
+  return <h1> Hello, {props.name} </h1>;
+}
 
-// class Welcome extends React.Component {
-//   render() {
-//     return <h1> Hello, {this.props.name} </h1>;
-//   }
-// }
+class Welcome extends React.Component {
+  render() {
+    return <h1> Hello, {this.props.name} </h1>;
+  }
+}
 
-// const element = <Welcome name="Sara" />
-// ReactDOM.render(
-//   element,
-//   document.getElementById('root')
-// )
+const element = <Welcome name="Sara" />
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+)
 
-// function Welcome(props) {
-//   return <h1>Hello, {props.name}</h1>;
-// }
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
 
-// function App() {
-//   return (
-//     <div>
-//       <Welcome name="Sara" />
-//       <Welcome name="Darius" />
-//       <Welcome name="Danny" />
-//     </div>
-//   );
-// }
+function App() {
+  return (
+    <div>
+      <Welcome name="Sara" />
+      <Welcome name="Darius" />
+      <Welcome name="Danny" />
+    </div>
+  );
+}
 
-// ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 
-// It is possible to write this way, but bit complicated
-// function formatDate(date) {
-//   return date.toLocaleDateString();
-// }
+It is possible to write this way, but bit complicated
+function formatDate(date) {
+  return date.toLocaleDateString();
+}
 
-// function Comment(props) {
-//   return (
-//     <div className="Comment">
-//       <div className="UserInfo">
-//         <img src="{props.author.avatorUrl}" alt="{props.author.name}" />
-//         <div className="UserInfo-name">{props.author.name}</div>
-//       </div>
-//       <div className="Comment-text">{props.text}</div>
-//       <div className="Comment-date">{formatDate(props.date)}</div>
-//     </div>
-//   );
-// }
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <div className="UserInfo">
+        <img src="{props.author.avatorUrl}" alt="{props.author.name}" />
+        <div className="UserInfo-name">{props.author.name}</div>
+      </div>
+      <div className="Comment-text">{props.text}</div>
+      <div className="Comment-date">{formatDate(props.date)}</div>
+    </div>
+  );
+}
 
-// const comment = {
-//   date: new Date(),
-//   text: "I hope you enjoy learning React!",
-//   author: {
-//     name: "Hello Kitty",
-//     avatarUrl: "https://placekitten.com/g/64/64"
-//   }
-// };
+const comment = {
+  date: new Date(),
+  text: "I hope you enjoy learning React!",
+  author: {
+    name: "Hello Kitty",
+    avatarUrl: "https://placekitten.com/g/64/64"
+  }
+};
 
-// ReactDOM.render(
-//   <Comment date={comment.date} text={comment.text} author={comment.author} />,
-//   document.getElementById("root")
-// );
+ReactDOM.render(
+  <Comment date={comment.date} text={comment.text} author={comment.author} />,
+  document.getElementById("root")
+);
 
-// bit extraction needed
+bit extraction needed
 
-// function formatDate(date) {
-//   return date.toLocaleDateString();
-// }
+function formatDate(date) {
+  return date.toLocaleDateString();
+}
 
-// function Avatar(props) {
-//   return (
-//     <img className="Avatar" src={props.user.avatarUrl} alt={props.user.name} />
-//   );
-// }
+function Avatar(props) {
+  return (
+    <img className="Avatar" src={props.user.avatarUrl} alt={props.user.name} />
+  );
+}
 
-// function UserInfo(props) {
-//   return (
-//     <div className="UserInfo">
-//       <Avatar user={props.user} />
-//       <div className="UserInfo-name">{props.user.name}</div>
-//     </div>
-//   );
-// }
+function UserInfo(props) {
+  return (
+    <div className="UserInfo">
+      <Avatar user={props.user} />
+      <div className="UserInfo-name">{props.user.name}</div>
+    </div>
+  );
+}
 
-// function Comment(props) {
-//   return (
-//     <div className="Comment">
-//       <UserInfo user={props.author} />
-//       <div className="Comment-text">{props.text}</div>
-//       <div className="Comment-date">{formatDate(props.date)}</div>
-//     </div>
-//   );
-// }
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <UserInfo user={props.author} />
+      <div className="Comment-text">{props.text}</div>
+      <div className="Comment-date">{formatDate(props.date)}</div>
+    </div>
+  );
+}
 
-// const comment = {
-//   date: new Date(),
-//   text: "I hope you enjoy learning React!",
-//   author: {
-//     name: "Hello Kitty",
-//     avatarUrl: "https://placekitten.com/g/64/64"
-//   }
-// };
-// ReactDOM.render(
-//   <Comment date={comment.date} text={comment.text} author={comment.author} />,
-//   document.getElementById("root")
-// );
+const comment = {
+  date: new Date(),
+  text: "I hope you enjoy learning React!",
+  author: {
+    name: "Hello Kitty",
+    avatarUrl: "https://placekitten.com/g/64/64"
+  }
+};
+ReactDOM.render(
+  <Comment date={comment.date} text={comment.text} author={comment.author} />,
+  document.getElementById("root")
+);
 
-// All React components must act like pure functions with respect to their props.
+All React components must act like pure functions with respect to their props.
 
-// function tick() {
-//   const element = (
-//     <div>
-//       <h1>Hello, World!</h1>
-//       <h2>It is {new Date().toLocaleDateString()}.</h2>
-//     </div>
-//   );
-//   ReactDOM.render(element, document.getElementById("root"));
-// }
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, World!</h1>
+      <h2>It is {new Date().toLocaleDateString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById("root"));
+}
 
-// setInterval(tick, 1000);
+setInterval(tick, 1000);
+
 function Clock(props) {
   return (
     <div>
@@ -211,200 +212,200 @@ class Clock extends React.Component {
   }
 }
 
-// Local state to class
+Local state to class
 
-// class Clock extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { date: new Date() };
-//   }
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
 
-//   render() {
-//     return (
-//       <div>
-//         <h1>Hello, world!</h1>
-//         <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-//       </div>
-//     );
-//   }
-// }
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
 
-// ReactDOM.render(<Clock />, document.getElementById("root"));
+ReactDOM.render(<Clock />, document.getElementById("root"));
 
-//States
-// function FormattedDate(props) {
-//   return <h2>It is {props.date.toLocaleTimeString()}.</h2>;
-// }
+States
+function FormattedDate(props) {
+  return <h2>It is {props.date.toLocaleTimeString()}.</h2>;
+}
 
-// class Clock extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { date: new Date() };
-//   }
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
 
-//   componentDidMount() {
-//     this.timerID = setInterval(() => this.tick(), 1000);
-//   }
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
 
-//   componentWillUnmount() {
-//     clearInterval(this.timerID);
-//   }
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
 
-//   tick() {
-//     this.setState({
-//       date: new Date()
-//     });
-//   }
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
 
-//   render() {
-//     return (
-//       <div>
-//         <h1>Hello, world!</h1>
-//         <FormattedDate date={this.state.date} />
-//       </div>
-//     );
-//   }
-// }
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <FormattedDate date={this.state.date} />
+      </div>
+    );
+  }
+}
 
-// ReactDOM.render(<Clock />, document.getElementById("root"));
+ReactDOM.render(<Clock />, document.getElementById("root"));
 
-// class Toggle extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { isToggleOn: true };
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isToggleOn: true };
 
-//     // This binding is necessary to make `this` work in the callback
-//     this.handleClick = this.handleClick.bind(this);
-//   }
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-//   handleClick() {
-//     this.setState(prevState => ({
-//       isToggleOn: !prevState.isToggleOn
-//     }));
-//   }
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
 
-//   render() {
-//     return (
-//       <button onClick={this.handleClick}>
-//         {this.state.isToggleOn ? "ON" : "OFF"}
-//       </button>
-//     );
-//   }
-// }
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? "ON" : "OFF"}
+      </button>
+    );
+  }
+}
 
-// ReactDOM.render(<Toggle />, document.getElementById("root"));
-// }
-// ​
-// function UserGreeting(props) {
-//   return <h1>Welcome back!</h1>;
-// }
-// ​
-// function GuestGreeting(props) {
-//   return <h1>Please sign up.</h1>;
-// }
-// ​
-// function Greeting(props) {
-//   const isLoggedIn = props.isLoggedIn;
-//   if (isLoggedIn) {
-//     return <UserGreeting />;
-//   }
-//   return <GuestGreeting />;
-// }
-// ​
-// function LoginButton(props) {
-//   return (
-//     <button onClick={props.onClick}>
-//       Login
-//     </button>
-//   );
-// }
-// ​
-// function LogoutButton(props) {
-//   return (
-//     <button onClick={props.onClick}>
-//       Logout
-//     </button>
-//   );
-// }
-// ​
-// ReactDOM.render(
-//   <LoginControl />,
-//   document.getElementById('root')
-// );
+ReactDOM.render(<Toggle />, document.getElementById("root"));
+}
+​
+function UserGreeting(props) {
+  return <h1>Welcome back!</h1>;
+}
+​
+function GuestGreeting(props) {
+  return <h1>Please sign up.</h1>;
+}
+​
+function Greeting(props) {
+  const isLoggedIn = props.isLoggedIn;
+  if (isLoggedIn) {
+    return <UserGreeting />;
+  }
+  return <GuestGreeting />;
+}
+​
+function LoginButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Login
+    </button>
+  );
+}
+​
+function LogoutButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Logout
+    </button>
+  );
+}
+​
+ReactDOM.render(
+  <LoginControl />,
+  document.getElementById('root')
+);
 
-// // List and Key
+// List and Key
 
-// function ListItem(props) {
-//   return <li>{props.value}</li>
-// }
+function ListItem(props) {
+  return <li>{props.value}</li>
+}
 
-// function NumberList(props) {
-//   const numbers = props.numbers
-//   return (
-//     <ul>
-//       {numbers.map((number) =>
-//       <ListItem key={number.toString()}
-//       value={number} />
-//       )}
-//       </ul>
-//   )
-// }
+function NumberList(props) {
+  const numbers = props.numbers
+  return (
+    <ul>
+      {numbers.map((number) =>
+      <ListItem key={number.toString()}
+      value={number} />
+      )}
+      </ul>
+  )
+}
 
-// const numbers = [1,2,3,4,5]
-// ReactDOM.render(
-//   <NumberList numers={numbers} />,
-//   document.getElementById('root')
-// )
+const numbers = [1,2,3,4,5]
+ReactDOM.render(
+  <NumberList numers={numbers} />,
+  document.getElementById('root')
+)
 
-// // Forms
+// Forms
 
-// class Reservation extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       isGoing: true,
-//       numberOfGuests: 2
-//     };
+class Reservation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isGoing: true,
+      numberOfGuests: 2
+    };
 
-//     this.handleInputChange = this.handleInputChange.bind(this);
-//   }
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
 
-//   handleInputChange(event) {
-//     const target = event.target;
-//     const value = target.type === 'checkbox' ? target.checked : target.value;
-//     const name = target.name;
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
 
-//     this.setState({
-//       [name]: value
-//     });
-//   }
+    this.setState({
+      [name]: value
+    });
+  }
 
-//   render() {
-//     return (
-//       <form>
-//         <label>
-//           Is going:
-//           <input
-//             name="isGoing"
-//             type="checkbox"
-//             checked={this.state.isGoing}
-//             onChange={this.handleInputChange} />
-//         </label>
-//         <br />
-//         <label>
-//           Number of guests:
-//           <input
-//             name="numberOfGuests"
-//             type="number"
-//             value={this.state.numberOfGuests}
-//             onChange={this.handleInputChange} />
-//         </label>
-//       </form>
-//     );
-//   }
-// }
+  render() {
+    return (
+      <form>
+        <label>
+          Is going:
+          <input
+            name="isGoing"
+            type="checkbox"
+            checked={this.state.isGoing}
+            onChange={this.handleInputChange} />
+        </label>
+        <br />
+        <label>
+          Number of guests:
+          <input
+            name="numberOfGuests"
+            type="number"
+            value={this.state.numberOfGuests}
+            onChange={this.handleInputChange} />
+        </label>
+      </form>
+    );
+  }
+}
 
-// ReactDOM.render(
-//   <Reservation />,
-//   document.getElementById('root')
-// );
+ReactDOM.render(
+  <Reservation />,
+  document.getElementById('root')
+);
